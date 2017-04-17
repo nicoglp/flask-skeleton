bceb8c8e54d28755e3eeaaaf61b8e3e845130c03
Gonzalo <gonza@MacBook-Air-de-Gonzalo.local>
2017-04-16 21:54:58 -0300

    DB model example.

--- app/kits/model.py
+++ app/kits/model.py
@@ -5,11 +5,29 @@ from sqlalchemy.types import *
 from app.base import model as base_model
 from app.phi import model as phi_model
 
+class KitTypeTest(base_model.BaseDBModel):
+    __tablename__ = 'kit_type_tests'
+
+    test = Column(String(128))
+    description = Column(UnicodeText)
+
+class KitTypeOther(base_model.BaseDBModel):
+    __tablename__ = 'kit_type_others'
+
+    text = Column(String(128))
+    description = Column(UnicodeText)
+
 class KitType(base_model.BaseDBModel):
     __tablename__ = 'kit_types'
 
     name = Column(String(128))
     description = Column(UnicodeText)
+
+    kit_type_test_id = Column('kit_type_test_id', String(32), ForeignKey('kit_type_tests.id'))
+    kit_type_test = relationship(KitTypeTest)
+
+    kit_type_other_id = Column('kit_type_other_id', String(32), ForeignKey('kit_type_others.id'))
+    kit_type_other = relationship(KitTypeOther)
 
 
 class Kit(phi_model.PHIModel):
@@ -21,4 +39,5 @@ class Kit(phi_model.PHIModel):
     barcode = Column('barcode', String(32))
     registered = Column('registered', DateTime)
     delivery_status = Column('delivery_status', String(32))
+
 
--- scripts/db/0000_init_schema.sql
+++ scripts/db/0000_init_schema.sql
@@ -3,11 +3,29 @@ CREATE EXTENSION IF NOT EXISTS pgcrypto;
 
 CREATE SCHEMA IF NOT EXISTS kits;
 
+CREATE TABLE kits.kit_type_tests (
+	id UUID DEFAULT gen_random_uuid() NOT NULL,
+	test VARCHAR(128),
+	description TEXT,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE kits.kit_type_others (
+	id UUID DEFAULT gen_random_uuid() NOT NULL,
+	text VARCHAR(128),
+	description TEXT,
+	PRIMARY KEY (id)
+);
+
 CREATE TABLE kits.kit_types (
 	id UUID DEFAULT gen_random_uuid() NOT NULL,
 	name VARCHAR(128),
 	description TEXT,
-	PRIMARY KEY (id)
+	kit_type_test_id UUID,
+	kit_type_other_id UUID,
+	PRIMARY KEY (id),
+	FOREIGN KEY(kit_type_test_id) REFERENCES kits.kit_type_tests (id),
+	FOREIGN KEY(kit_type_other_id) REFERENCES kits.kit_type_others (id)
 );
 
 CREATE TABLE kits.kits (
